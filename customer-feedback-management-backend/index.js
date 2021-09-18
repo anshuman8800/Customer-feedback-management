dba8c6c897601c4edce48951d3092de6718222a0
Anshuman <anshuman8800@gmail.com>
2021-09-18 22:04:30 +0530

    Add login and register page in backend

--- customer-feedback-management-backend/index.js
+++ customer-feedback-management-backend/index.js
@@ -1,21 +1,50 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const createError = require("http-errors");
+const path = require("path");
+
+const loginRouter = require("./routes/login");
+const registerRouter = require("./routes/register");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/myCostomerFeedbackDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, () => {
-    console.log("DB connected")
-})
+app.use("/login", loginRouter);
+app.use("/register", registerRouter);
+
+app.get("/", function (req, res) {
+  res.send("MY API");
+});
+
+main().catch(err => console.log(err)); 
+async function main() {
+  await mongoose.connect(
+    "mongodb://localhost:27017/myCostomerFeedbackDB",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    () => {
+      console.log("DB connected");
+    }
+  );
+}
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-})
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema)
+
+
+
+app.listen(8000, () => {
+  console.log("Using dynamic port ", 8000);
+});
+
+module.exports = app;
