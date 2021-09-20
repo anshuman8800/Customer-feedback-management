05f85d5c4d73032ac8b42d50ba1592ec346faa39
Anshuman <anshuman8800@gmail.com>
2021-09-20 18:54:26 +0530

    Connect api call of login page

--- customer-feedback-management-backend/routes/login.js
+++ customer-feedback-management-backend/routes/login.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const database = require("../database");
 
 /* GET home page. */
-router.post("/", function (req, res) {
-  res.send("post req of login page");
+router.post("/", async function (req, res) {
+  console.log(req.body);
+  const { email, password } = req.body;
+  try {
+    const user = await database.select().from("Login").where("email", email);
+    if (user.length == 0) {
+      console.log("Wrong email");
+    } else if (user[0].password != password) {
+      console.log("Wrong password");
+    } else {
+      console.log("successfully login");
+      res.send("success");
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.get("/", function (req, res) {
--- customer-feedback-management/src/components/login/login.js
+++ customer-feedback-management/src/components/login/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./login.css";
+import axios from "axios";
 
 const Login = () => {
   const [user, setUser] = useState({
@@ -15,13 +16,24 @@ const Login = () => {
     });
   };
 
+  const login= () => {
+    const { email, password} = user;
+    if (email && password) {
+      alert("send");
+      axios.post("http://localhost:8000/login", user).then((res) => {
+        // history.push("/login");
+      });
+    } else {
+      alert("invlid input");
+    }
+  };
   return (
     <div className="login">
       {console.log(user)}
       <h1>Login</h1>
       <input type="text" name="email" value={user.email} onChange={handleChange} placeholder="Enter your Email"></input>
       <input type="password" name="password" value={user.password} onChange={handleChange}  placeholder="Enter your Password"></input>
-      <div className="button">Login</div>
+      <div className="button" onClick={login}>Login</div>
       <div>or</div>
       <div className="button">Register</div>
     </div>
