8a5ba2896fa032e8c07b09b38c5de0f263a2c59b
Anshuman <anshuman8800@gmail.com>
2021-09-20 22:07:58 +0530

    Implement logout

--- frontend/src/components/homepage/homepage.js
+++ frontend/src/components/homepage/homepage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./homepage.css";
+import { useHistory } from "react-router-dom"
 
 const Homepage = ({user}) => {
+  const history = useHistory()
   return (
     <div className="homepage">
       <h1>Homepage</h1>
-      <div className="button">LogOut</div>
+      <div className="button" onClick={() => history.push("/login")}>LogOut</div>
       <h1>{user[0].name}</h1>
       {console.log(user)}
     </div>
